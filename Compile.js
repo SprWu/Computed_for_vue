b5520973dfd4deae4ae64736a2fac36ed2a42e72
sup.me <1035924544@qq.com>
2019-04-04 10:13:22 +0800

    解析指令功能

--- Compile.js
+++ Compile.js
@@ -1,11 +1,11 @@
 nodeToFragment(el) {
     var child,
-fragment = document.createDocumentFragment();// 创建fragment
+        fragment = document.createDocumentFragment(); // 创建fragment
     while (child = el.firstChild) { // 遍历节点
         fragment.appendChild(child); // 创建fragment
     }
     return fragment;
-} 
+}
 
 isTextNode(node) {
     return node.nodeType === 3;
@@ -21,10 +21,9 @@ compileElement(el) {
     [].slice.call(childNodes).forEach(node => {
         var text = node.textContent;
         /* 判断是DOM节点还是文本节点 */
-        if(this.isElementNode(node)) {
+        if (this.isElementNode(node)) {
             // 进行指令解析
-        }
-        else if (this.isTextNode(node) && reg.test(text)) {
+        } else if (this.isTextNode(node) && reg.test(text)) {
             //exec() 方法用于检索字符串中的正则表达式的匹配。
             //返回一个数组，其中存放匹配的结果。如果未找到匹配，则返回值为 null。
             this.compileText(node, reg.exec(text)[1].trim());
@@ -45,7 +44,63 @@ compileText(node, key) {
 }
 // 文本节点 更新视图/初始化 函数
 updateText(node, value) {
-// textContent 属性设置或返回指定节点的文本内容
+    // textContent 属性设置或返回指定节点的文本内容
     node.textContent = typeof value == 'undefined' ? '' : value;
 }
 
+isDirective(attr) {
+    return attr.indexOf('v-') === 0;
+}
+isEventDirective(dir) {
+    return dir.indexOf('on:') === 0;
+}
+
+
+compile(node) {
+    var nodeAttrs = node.attributes; //attributes 属性返回指定节点的属性集合，即 NamedNodeMap
+    var self = this;
+    //Array.prototype属性表示Array构造函数的原型，并允许为所有Array对象添加新的属性和方法。
+    //Array.prototype本身就是一个Array
+    Array.prototype.forEach.call(nodeAttrs, function (attr) {
+        //添加事件的方法名和前缀:
+        // v-on:click="onClick" ,则attrName = 'v-on:click' id="app" attrname= 'id'
+        var attrName = attr.name;
+        if (self.isDirective(attrName)) { //如果是指令：'v-'开头
+            //添加事件的方法名和前缀:v-on:click="onClick" ,exp = 'onClick'
+            var exp = attr.value;
+            var dir = attrName.substring(2); // 去除 'v-'
+            if (self.isEventDirective(dir)) { // 事件指令解析
+                self.compileEvent(node, self.vm, exp, dir);
+            } else { // model指令解析
+                self.compileModel(node, self.vm, exp, dir);
+            }
+            node.removeAttribute(attrName); // 解析完成，移除该属性
+        }
+    })
+}
+compileModel(node, vm, exp, dir) {
+    var self = this;
+    var val = this.vm[exp]; // this.data[exp];
+    this.modelUpdater(node, val); // 更新函数
+    new Watcher(this.vm, exp, function (value) {
+        self.modelUpdater(node, value);
+    });
+    /* 监听input的输入操作 */
+    node.addEventListener('input', function (e) {
+        var newValue = e.target.value; // 获取输入框的最新内容
+        if (val === newValue) return;
+        self.vm[exp] = newValue; // SelfVue.data[exp] = newValue
+        val = newValue; // 旧值更新
+    })
+}
+compileEvent(node, vm, exp, dir) {
+    // 获取事件名, v-on:click => click
+    let eventType = dir.split(':')[1];
+    let cb = vm.methods && vm.methods[exp]; // 是否声明了methods
+    if (eventType && cb) {
+        node.addEventListener(eventType, cb.bind(vm), false);
+    }
+}
+modelUpdater(node, value) {
+    node.value = typeof value === 'undefined' ? '' : value;
+}
